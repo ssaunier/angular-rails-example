74e82b7e9fe5ccebbe1e26aaeb31b1af5849e312
Sebastien Saunier <seb@saunier.me>
2014-02-04 12:11:08 +0100

    rails new -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularRailsExample::Application.config.secret_key_base = 'be5eda9603ae7eb880ab34c48df8a043ae23165f4131cb7caf4de079461ff6c67eff4558eaccd97a5a6f2608bf93a383698f2b990bad24b851ccfb173002f046'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AngularRailsExample::Application.config.session_store :cookie_store, key: '_angular-rails-example_session'
