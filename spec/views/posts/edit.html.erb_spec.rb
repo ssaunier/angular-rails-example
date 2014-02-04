7f608f9fc4ba4ea7a265c3c73b73b2d718842663
Sebastien Saunier <seb@saunier.me>
2014-02-04 12:19:14 +0100

    Scaffold a Post model

---
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "posts/edit" do
+  before(:each) do
+    @post = assign(:post, stub_model(Post,
+      :title => "MyString"
+    ))
+  end
+
+  it "renders the edit post form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form[action=?][method=?]", post_path(@post), "post" do
+      assert_select "input#post_title[name=?]", "post[title]"
+    end
+  end
+end
